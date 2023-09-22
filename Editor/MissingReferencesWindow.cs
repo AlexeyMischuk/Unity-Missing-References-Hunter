3e6f56a00f68d2152c9c5ab1387507a0f704db44
Alexey Mischuk <gcbkjc@gmail.com>
2023-09-23 01:37:58 +0300

    basic window layout

--- Editor/MissingReferencesWindow.cs
+++ Editor/MissingReferencesWindow.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class MIssingReferencesWindow : MonoBehaviour
+public class MissingReferencesWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _scrollPosition;
+    private Object _source;
+    
+    [MenuItem("Window/Missing References Hunter")]
+    public static void ShowWindow()
     {
-        
+        GetWindow<MissingReferencesWindow>("Missing References Hunter");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnGUI()
     {
+        GUILayout.Space(30);
         
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        var buttonStyle = new GUIStyle(GUI.skin.button)
+        {
+            fixedWidth = 0,
+            stretchWidth = true
+        };
+        if (GUILayout.Button("Find Missing References", buttonStyle, GUILayout.MaxWidth(300)))
+        {
+            Debug.Log("Button clicked!");
+        }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        
+        GUILayout.Space(30);
+
+        _scrollPosition = GUILayout.BeginScrollView(
+            _scrollPosition, GUILayout.MaxWidth(position.width-10));
+        GUILayout.BeginVertical();
+        for (var i = 1; i <= 50; i++)
+        {
+            GUILayout.BeginHorizontal();
+            _source = EditorGUILayout.ObjectField(_source, typeof(Object), true, GUILayout.Width(250));
+            GUILayout.Label("asset path");
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+        GUILayout.EndScrollView();
     }
 }
