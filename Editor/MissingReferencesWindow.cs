d1a777dc5367ec6ffcfef1ec0dc1d7c99413f19e
Alexey Mischuk <gcbkjc@gmail.com>
2023-09-27 22:18:03 +0300

    page buttons update content inside scroll view

--- Editor/MissingReferencesWindow.cs
+++ Editor/MissingReferencesWindow.cs
@@ -1,9 +1,15 @@
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class MissingReferencesWindow : EditorWindow
 {
     private Vector2 _scrollPosition;
+    private int _currentPage;
+    private int _totalPages;
+    private bool _isSearchFinished;
+
+    private const int ItemsPerPage = 10;
     
     [MenuItem("Window/Missing References Hunter")]
     public static void ShowWindow()
@@ -25,30 +31,74 @@ public class MissingReferencesWindow : EditorWindow
         if (GUILayout.Button("Find Missing References", buttonStyle, GUILayout.MaxWidth(300)))
         {
             AssetSearch.FindAllAssets();
+            _totalPages = Mathf.CeilToInt((float)AssetSearch.AssetArray.Length / ItemsPerPage);
+            _isSearchFinished = true;
         }
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
         
-        GUILayout.Space(30);
+        GUILayout.Space(10);
 
-        _scrollPosition = GUILayout.BeginScrollView(
-            _scrollPosition, GUILayout.MaxWidth(position.width-10));
-        GUILayout.BeginVertical();
-        for (var i = 1; i < AssetSearch.AssetArray.Length; i++)
+        if (_isSearchFinished)
         {
-            var assetObject = RestoreObjectFromGuid(AssetSearch.AssetArray[i]);
+            var pageButtonStyle = new GUIStyle(GUI.skin.button)
+            {
+                fixedWidth = 30
+            };
+            
             GUILayout.BeginHorizontal();
-            EditorGUILayout.ObjectField(assetObject, typeof(Object), true, GUILayout.Width(250));
-            GUILayout.Label("asset path");
+            for (var i = 1; i <= _totalPages; i++)
+            {
+                if (GUILayout.Button($"{i}", pageButtonStyle))
+                {
+                    _currentPage = i - 1;
+                }
+            }
             GUILayout.EndHorizontal();
+            
+            
+            _scrollPosition = GUILayout.BeginScrollView(
+                _scrollPosition, GUILayout.MaxWidth(position.width-10));
+            GUILayout.BeginVertical();
+            if (_currentPage == 0)
+            {
+                for (var j = 0; j < ItemsPerPage; j++)
+                {
+                    CreateContentRow(j);
+                }
+            }
+            else
+            {
+                var startIndex = _currentPage * ItemsPerPage;
+                var endIndex = startIndex + ItemsPerPage;
+                if (_currentPage == _totalPages-1)
+                {
+                    endIndex = AssetSearch.AssetArray.Length;
+                }
+                for (var j = startIndex; j < endIndex; j++)
+                {
+                    CreateContentRow(j);
+                }
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndScrollView();
+            GUILayout.Space(5);
         }
-        GUILayout.EndVertical();
-        GUILayout.EndScrollView();
     }
 
-    private Object RestoreObjectFromGuid(string guid)
+    private static Object RestoreObjectFromGuid(string guid)
     {
         var assetPath = AssetDatabase.GUIDToAssetPath(guid);
         return AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+    }
+
+    private static void CreateContentRow(int assetIndex)
+    {
+        var assetGuid = AssetSearch.AssetArray[assetIndex];
+        var assetObject = RestoreObjectFromGuid(assetGuid);
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.ObjectField(assetObject, typeof(Object), true, GUILayout.Width(250));
+        GUILayout.Label("asset path");
+        GUILayout.EndHorizontal();  
     }
 }
